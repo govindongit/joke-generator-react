32c893900ffa982f42b87fc7a9d917ae9efd246e
govindongit <govindnag5863@gmail.com>
2025-01-07 13:03:39 +0530

    First Commit Joke Generator

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Jokegen from './component/Jokegen/Jokegen';
+
+
+function App() {
+  return (
+    <div>
+   <div style={{textAlign:"center"}}><h2>Random Joke Generator In the React </h2></div>
+   <div>
+    <Jokegen/>
+   </div>
+    </div>
+  );
+}
+
+export default App;
